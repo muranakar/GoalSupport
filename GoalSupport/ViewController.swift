f306e6d80b34dbd0607d28e8fe83b8638eefe8aa
muranakar <P1211065@gmail.com>
2022-04-13 22:04:53 +0900

    Initial Commit

---
+++ GoalSupport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoalSupport
+//
+//  Created by 村中令 on 2022/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
